03573642d2e39b40b23cebdad94af8ad73902031
rizhkh <mrk_rizwan@hotmail.com>
2023-09-24 14:44:53 -0400

    completed

--- src/pages/HeaderNameInfo.js
+++ src/pages/HeaderNameInfo.js
@@ -3,27 +3,30 @@ import '../css/stylesforproject.css';
 import '../css/hovereffect.css';
 
 export default function HeaderNameInfo(){
-  return <><table style= {{width:"100%"}}>
-  <tr>
-  <td>
+  return <>
+  <table style= {{width:"100%"}}>
+    <tr>
+      <td>
+        <tr>                  
+          <h1 style={{ fontFamily: "Open Sans,sans-serif", paddingBottom: "1px", 
+          margin: "5px 0 1px", borderBottom: "3px solid #eee", width: '100%'}}>
 
-  <tr>                  
+          <b>
+          R<text style={{ fontSize:20}}>IZWAN </text>
+          K<text style={{ fontSize:20}}>HAN</text>
+            </b>    
 
-    <h1 style={{ fontFamily: "Open Sans,sans-serif", paddingBottom: "6px", 
-    margin: "5px 0 20px", borderBottom: "3px solid #eee", width: '100%'}}>
-    <b>Rizwan Khan </b>     
-    <text 
-    style={{ 
-      fontFamily: "Open Sans,sans-serif", 
-      paddingBottom: "6px", 
-    margin: "5px 0 20px", 
-    width: '100%',
-    fontWeight: "500",
-  fontSize:14}}
-    ><b><text style={{fontSize:11}}>@</text> Full-Stack Engineer</b></text>
-    </h1>
+          <text style={{ fontFamily: "Open Sans,sans-serif", paddingBottom: "6px", 
+          margin: "5px 0 20px", width: '100%', fontWeight: "500", fontSize:14}}>
+            <b>
+              <text style={{fontSize:11}}>@</text>
+              <text style={{fontSize:15}}> Full-Stack Engineer</text>
+            </b>
+          </text>
+          </h1>
+        </tr>
+      </td>
     </tr>
-  </td>
-  </tr>
-</table></>;
+  </table>
+  </>;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import React, { useEffect, useState } from "react";
 import '../css/styles.css';
 import '../css/stylesforproject.css';
 import '../css/hovereffect.css';
-import fewProjects from "../project_min";
 import instagram from '../img/instagram.png';
 import git from '../img/git.png';
 import ln from '../img/ln.png';
@@ -13,67 +12,55 @@ import Links from "../pages/Links";
 import HeaderNameInfo from "../pages/HeaderNameInfo";
 import HomeAboutMeStatement from "../pages/HomeAboutMeStatement";
 import SkillStackClickAbleButtons from "../pages/SkillStack";
+import SampleProjects from "../pages/SampleProjects";
+
 
 export default function Home() {
   return<>
-  <div 
-  style={{ display: "flex",  flexDirection:"row",
-  position: "relative"}}
-  > 
+  <div class="topnav">
+    <div class="topnavlink">
+    <div class="linkBox">
+        <a href="https://www.instagram.com/rizhkhan/"><img src={instagram} width="15" height="15" /></a>
+      </div>
 
-      <div>
+      <div class="linkBox">
+      <a href="https://github.com/rizhkh"><img class="icons" src={git} width="15" height="15" /></a>
+      </div>
+
+      <div class="linkBox">
+        <a href="https://www.linkedin.com/in/rizhkh/"><img src={ln} width="15" height="15" /></a> 
+      </div>
+    </div>
+
+    <div class="topnavdetail">
+        <div class="label label-tab" style={{backgroundColor: 'transparent'}}>
+          <text style={{ color: 'gray'}}>R<text style={{ fontSize:9}}>IZWAN </text></text>
+          K<text style={{ fontSize:9}}>HAN ©2023</text>
+          </div>
+    </div>
+
+  </div>
+
+
+
+  <br />
+  <div style={{ display: "flex",  flexDirection:"row", position: "relative"}}> 
+    <div>
       <HeaderNameInfo />
       <HomeAboutMeStatement />
-
       <br />
       <SkillStackClickAbleButtons />
       <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      { FooterPage() }
+    </div>
+
+    <div style={{ padding: "2%"}}>
+      <div style={{ backgroundColor: 'yellow', position: "relative"}}>
+        <Links />
       </div>
-
-
-    <div style={{ 
-  padding: "2%"
-  }}>
-    <div style={{ 
-      backgroundColor: 'yellow',
-      position: "relative"
-
-    }}>
-
-
-    <Links />
-
+      <br />   
+      <SampleProjects />
     </div>
-    <br />
-    
-      {fewProjects()}
-    </div>
+
   </div>
   </>;
-}
-
-
-function FooterPage()
-{
-
-return <>      
-<footer style={{
-  color: "black",
-  padding: "10px",
-  position: "absolute",
-  bottom: "0",
-  width: "100%"
-  }}>
-
- ©2023 Rizwan 
-<a href="https://www.instagram.com/rizhkhan/"><img src={instagram} width="27" height="27" /></a>
-<a href="https://github.com/rizhkh"><img class="icons" src={git} width="27" height="27" /></a>
-<a href="https://www.linkedin.com/in/rizhkh/"><img src={ln} width="27" height="27" /></a> 
-</footer></>
-
 }
